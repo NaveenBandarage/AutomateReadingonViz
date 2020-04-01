0465867ab21b6bce61e064a6af4b9922807098e9
NaveenBandarage <thebanterage@gmail.com>
2020-04-01 14:46:41 +1300

    decided to use dictionaries.

--- Viz.py
+++ Viz.py
@@ -1,9 +1,28 @@
 from selenium import webdriver
 print("This is a test to see if import works. ")
 browser = webdriver.Chrome("/Users/naveenbandarage/Downloads/chromedriver")
+
+
+#This method works to get 
 browser.get("https://www.viz.com/")
+
 elem = browser.find_element_by_xpath('//*[@id="curtain"]/header/div/nav/ul[1]/li[3]/a')
 print(elem.text)
 elem.click()
-mangaSeries = browser.find_elements_by_link_text("Mitama Security: Spirit Busters")
-print(mangaSeries)
+
+print("What series do you want to read?")
+seriesRead = input("Series: ")
+print(seriesRead)
+
+mangaDictionary = {}
+# mangaSeries = browser.find_elements_by_link_text("My Hero Academia: Vigilantes")
+# mangaSeries.click()
+# print(mangaSeries)
+
+
+userInput = input("Do you want to quit? (y/n): ")
+
+print("User input is: " + str(userInput))
+#quitting works need to add delay. 
+if userInput == "y":
+    browser.close()
