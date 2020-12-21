e3d9c1fe2ddc19bb0383c71a58bbd9580a656bd3
Restyled.io <commits@restyled.io>
2020-12-21 22:02:41 +0000

    Restyled by isort

--- Viz.py
+++ Viz.py
@@ -1,7 +1,8 @@
-from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.keys import Keys
 import difflib
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
 
 booleanA = True
 readingBool = True
