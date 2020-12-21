412beeb958d7d349879f9373b6a76e1901f875e4
Restyled.io <commits@restyled.io>
2020-12-21 22:02:39 +0000

    Restyled by black

--- Viz.py
+++ Viz.py
@@ -5,22 +5,42 @@ import difflib
 
 booleanA = True
 readingBool = True
-while(booleanA):
+while booleanA:
     print("Welcome to the Viz automated reader!\n")
-    mangaNameDictionary = {"One Piece": "one-piece", "Kaiju No 8": "kaiju-no-8", "Mashle": "mashle", "Jujutsu Kaisen": "jujutsu-kaisen", "Black Clover": "black-clover", "Chainsaw Man": "chainsaw-man", "We never learn": "we-never-learn", "Undead Unlock": "undead-unluck", "Agravity Boys": "agravity-boys", "Ayakashi Triangle": "ayakashi-triangle", "Dr Stone": "dr-stone",
-                           "Hard Boiled Cop and Dolphin": "hard-boiled-cop-and-dolphin", "Magu Chan: God of Destruction": "magu-chan-god-of-destruction", "Mission: Yozakura Family": "mission-yozakura-family", "Moriking": "moriking", "Our Blood Oath": "our-blood-oath", "Phantom Seer": "phantom-seer", "My Hero Academia": "my-hero-academia", "Spy x Family": "spy-x-family"}
+    mangaNameDictionary = {
+        "One Piece": "one-piece",
+        "Kaiju No 8": "kaiju-no-8",
+        "Mashle": "mashle",
+        "Jujutsu Kaisen": "jujutsu-kaisen",
+        "Black Clover": "black-clover",
+        "Chainsaw Man": "chainsaw-man",
+        "We never learn": "we-never-learn",
+        "Undead Unlock": "undead-unluck",
+        "Agravity Boys": "agravity-boys",
+        "Ayakashi Triangle": "ayakashi-triangle",
+        "Dr Stone": "dr-stone",
+        "Hard Boiled Cop and Dolphin": "hard-boiled-cop-and-dolphin",
+        "Magu Chan: God of Destruction": "magu-chan-god-of-destruction",
+        "Mission: Yozakura Family": "mission-yozakura-family",
+        "Moriking": "moriking",
+        "Our Blood Oath": "our-blood-oath",
+        "Phantom Seer": "phantom-seer",
+        "My Hero Academia": "my-hero-academia",
+        "Spy x Family": "spy-x-family",
+    }
 
     print("These are all the series to read:")
     for x in mangaNameDictionary:
         print(x)
     boolean = True
-    while(boolean):
+    while boolean:
         print("\nWhat series do you want to read? (Must match names printed above)")
         seriesRead = input("Series: ")
 
         closestWord = difflib.get_close_matches(
-            seriesRead, list(mangaNameDictionary.keys()), 1, 0.4)
-        if(len(closestWord) != 0):
+            seriesRead, list(mangaNameDictionary.keys()), 1, 0.4
+        )
+        if len(closestWord) != 0:
             print("The series is: " + closestWord[0])
             boolean = False
         else:
@@ -30,13 +50,14 @@ while(booleanA):
     print("Key Value is =:" + mangaNameDictionary.get(closestWord[0]) + "\n")
     mangaName = mangaNameDictionary.get(closestWord[0])
     browser = webdriver.Chrome(
-        executable_path=r'/Users/naveenbandarage/Desktop/OtherProjects/PythonProjects/SeleniumViz/chromedriver')
+        executable_path=r"/Users/naveenbandarage/Desktop/OtherProjects/PythonProjects/SeleniumViz/chromedriver"
+    )
 
     # This method works to get
     browser.get("https://www.viz.com/shonenjump/chapters/" + mangaName)
-    if(browser.find_element_by_class_name('icon-close')):
+    if browser.find_element_by_class_name("icon-close"):
         print("There exists an icon...")
-        elem = browser.find_element_by_class_name('icon-close')
+        elem = browser.find_element_by_class_name("icon-close")
         browser.implicitly_wait(30)
         print("There exists an icon after waiting...")
         elem = browser.find_element_by_xpath('//*[@id="modal-follow"]/a/i')
@@ -45,11 +66,11 @@ while(booleanA):
     # elem = browser.find_element_by_xpath('//*[@id="section0"]/div/div[2]/div[2]/a/div[2]/table/tbody/tr/td[2]/div/span/i')
     # elem = browser.find_element_by_xpath('//*[@id="section0"]/div/div[2]/div[2]/a/div[2]')
     elem = browser.find_element_by_xpath(
-        '//*[@id="section0"]/div/div[2]/div[2]/a/div[2]/table/tbody/tr/td[2]/div/span')
+        '//*[@id="section0"]/div/div[2]/div[2]/a/div[2]/table/tbody/tr/td[2]/div/span'
+    )
     elem.click()
 
-    fullScreen = browser.find_element_by_xpath(
-        '//*[@id="reader_tools"]/a[6]/i')
+    fullScreen = browser.find_element_by_xpath('//*[@id="reader_tools"]/a[6]/i')
 
     browser.implicitly_wait(20)
 
